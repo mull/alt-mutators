7bed17c9d1b690b4958c82fda98bff93c694d5d8
Emil Ahlbäck <e.ahlback@gmail.com>
2015-08-25 10:39:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction
+`alt-mutators` are a simple way to define mutating operations for an API, tied to your alt store. Their interface bear a striking similarity to [alt's data async sources](http://alt.js.org/docs/async/), in fact the code is based based on just that.
+
+# Usage
+Sorry, to come. :grin:
---
+++ src/index.js
@@ -0,0 +1,63 @@
+import * as fn from 'alt/utils/functions'
+
+function registerMutator(mutatorDef) {
+  let loadCounter = 0
+
+  const asyncMethods = fn.isFunction(mutatorDef)
+    ? mutatorDef(this.alt)
+    : mutatorDef
+
+  const toExport = Object.keys(asyncMethods).reduce((publicMethods, methodName) => {
+    const desc = asyncMethods[methodName]
+    const spec = fn.isFunction(desc) ? desc(this) : desc
+
+    const validHandlers = ['success', 'error', 'writing']
+    validHandlers.forEach((handler) => {
+      if (spec[handler] && !spec[handler].id) {
+        throw new Error(`${handler} handler must be an action function`)
+      }
+    })
+
+    publicMethods[methodName] = (args) => {
+      if (args === Object(args)) {
+        throw new Error(`${methodName} only accepts an object as its sole argument`)
+      }
+
+      args.state = this.getInstance().getState()
+
+      const makeActionHandler = (action, isError) => {
+        return (x) => {
+          const fire = () => {
+            loadCounter -= 1
+            action(x, action, args)
+            if (isError) throw x
+          }
+          return this.alt.trapAsync ? (() => fire()) : fire()
+        }
+      }
+
+      loadCounter += 1
+      spec.writing(args)
+
+      let currentState = this.getInstance().getState()
+      args.state = currentState
+
+      return spec.write(args).then(
+        makeActionHandler(spec.success),
+        makeActionHandler(spec.error, 1)
+      )
+    }
+
+    return publicMethods
+  }, {})
+
+  this.exportPublicMethods(toExport)
+  this.exportPublicMethods({
+    isLoading: () => loadCounter > 0
+  })
+}
+
+export default function Mutator(StoreModel) {
+  StoreModel.prototype.registerMutator = registerMutator
+  return StoreModel
+}
